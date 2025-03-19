50447b04d656674492e0d1d3cb31c75056b35283
samirul <samirulhaque08@gmail.com>
2025-03-19 12:39:06 +0530

    Added toggle background mode and bootstrap navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import useLocalStorage from "use-local-storage";
+import {Toggle} from '../components/Toggle';
+import Navbars from '../components/Navbars';
 
 
 function App() {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+  console.log(isDark)
   return (
     <>
-      <div><h1>Hello World!</h1></div>
+    
+      <Navbars value={isDark}/>
+      <div className="App" data-theme={isDark ? "dark" : "light"}>
+      <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+    </div>
     </>
   )
 }
