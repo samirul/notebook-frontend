84e5dfa9a160296475c4262650bc602337335692
samirul Haque <samirulhaque08@gmail.com>
2025-06-11 13:22:04 +0530

    Add react-select for category selection in NewNotes component and fetch categories from API.

--- pages/NewNotes.jsx
+++ pages/NewNotes.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactQuill from 'react-quill-new';
 import 'react-quill-new/dist/quill.snow.css';
 import 'react-quill-new/dist/quill.bubble.css';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import Select from 'react-select'
 
 const NewNotes = () => {
-    const [formData, setFormData] = useState({ name: '', options: '', description: '' });
+    const [categories, setCategories] = useState([])
+    const [formData, setFormData] = useState({ title: '', category: '', note_text: '' });
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     const handleQuillChange = (value) => {
-        setFormData({ ...formData, description: value });
+        setFormData({ ...formData, note_text: value });
     };
 
-    const handleSubmit = (e) => {
+    const fetchCategories = async () => {
+        const response = await axios.get("http://localhost:8000/api/notes/categories/", { withCredentials: true }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        })
+        setCategories(response.data);
+    }
+
+    const options = categories.map(item => ({
+        value: item.id,
+        label: item.title
+    }));
+
+    useEffect(() => {
+        fetchCategories();
+    }, [])
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('Form submitted:', formData);
+        try {
+            await axios.post("http://localhost:8000/api/notes/new-note/",
+                formData,
+                { withCredentials: true }, {
+                headers:
+                {
+                    'X-CSRFToken': Cookies.get('csrftoken'),
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+
+            })
+        } catch (error) { }
     };
 
     const modules = {
@@ -52,38 +87,38 @@ const NewNotes = () => {
                             <Form.Label>Title</Form.Label>
                             <Form.Control
                                 type="text"
-                                name="name"
-                                value={formData.name}
+                                name="title"
+                                value={formData.title}
                                 onChange={handleChange}
                                 placeholder="Enter your title"
                             />
                         </Form.Group>
 
-                        <Form.Group className="mb-3 from-size-item" controlId="formOptions">
+                        <Form.Group className="mb-3 from-size-item" controlId="formCategory">
                             <Form.Label>Category</Form.Label>
-                            <Form.Select
-                                name="options"
-                                value={formData.options}
-                                onChange={handleChange}
+                            <Select
+                                name="category"
+                                value={options.find(opt => opt.value === formData.category)}
+                                onChange={(selectedOption) => handleChange({
+                                    target: { name: 'category', value: selectedOption.value }
+  })}
+                                options={options}
+                                placeholder="Select a category"
                             >
-                                <option value="">Select a category</option>
-                                <option value="option1">Option 1</option>
-                                <option value="option2">Option 2</option>
-                                <option value="option3">Option 3</option>
-                            </Form.Select>
+                            </Select>
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formDescription">
+                        <Form.Group className="mb-3" controlId="formNoteText">
                             <ReactQuill theme="snow"
-                                value={formData.description}
+                                value={formData.note_text}
                                 onChange={handleQuillChange}
                                 modules={modules}
                                 className='react-quill-class' />
                         </Form.Group>
                         <div className='btn-container'>
-                        <button className="button-submit" role="button">
-                            Submit
-                        </button>
+                            <button className="button-submit" role="button">
+                                Submit
+                            </button>
                         </div>
                     </Form>
                 </div>
