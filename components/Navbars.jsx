81b75f9af3f88f86fc9bbd94071d0d4ce9c3add8
samirul Haque <samirulhaque08@gmail.com>
2025-08-13 21:10:17 +0530

    Added logout functionality in navbar.

--- components/Navbars.jsx
+++ components/Navbars.jsx
@@ -1,11 +1,52 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Toggle} from '../components/Toggle';
+import { Toggle } from '../components/Toggle';
+import { CheckUser } from './CheckUser';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
-const Navbars = ({value, handleChange}) => {
+const Navbars = ({ value, handleChange }) => {
+  const [user, setUser] = useState([])
+
+  const getUser = async () => {
+    try {
+      const response = await CheckUser()
+      if (response?.data && response?.status === 200) {
+        setUser(response?.data?.user)
+      }
+    } catch (error) { }
+  }
+  useEffect(() => {
+    getUser();
+  }, [])
+  const handleLogout = async () => {
+    try {
+      const response = await CheckUser()
+      if (response?.data && response?.status === 200) {
+        try {
+          await axios.post("http://localhost:8000/api/auth/logout/", {
+            headers: {
+              'X-CSRFToken': Cookies.get('csrftoken'),
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            }
+          }, { withCredentials: true });
+          window.location.replace("/login");
+        } catch (error) {
+          if (error.status === 401) {
+            window.location.replace("/login");
+          }
+        }
+      }
+    } catch (error) {
+      if (error.status === 401) {
+        window.location.replace("/login");
+      }
+    }
+  }
   return (
     <>
       <div>
@@ -16,7 +57,8 @@ const Navbars = ({value, handleChange}) => {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="nav-bar-link me-auto justify-content-end flex-grow-1">
                 <Nav.Link className='navlink-text-notes' href="/notes">Notes</Nav.Link>
-                <Nav.Link className='navlink-text-login' href="/login">Login</Nav.Link>
+                {user?.id ? <Nav.Link className='navlink-text-login' type='button' onClick={handleLogout}>Logout</Nav.Link> :
+                  <Nav.Link className='navlink-text-login' href="/login">Login</Nav.Link>}
                 <Toggle isChecked={value} handleChange={handleChange} />
               </Nav>
             </Navbar.Collapse>
